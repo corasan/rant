9ed5df5d673ccd459c1d844d75ccbebaeb69dc33
Henry Paulino <henrypl360@gmail.com>
2020-11-06 21:50:57 -0500

    Initial Commit

---
+++ Rant/RantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RantApp.swift
+//  Rant
+//
+//  Created by Henry Paulino on 11/6/20.
+//
+
+import SwiftUI
+
+@main
+struct RantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
